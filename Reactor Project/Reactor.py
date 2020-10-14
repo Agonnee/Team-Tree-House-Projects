f361f361c1dd27919ac652f7438bad38ea9a69eb
Agonnee <67187469+Agonnee@users.noreply.github.com>
2020-10-13 19:30:23 -0700

    This title is the test commit while failing to understand branching
    I will figure this out through documentation and trial and error tomorrow.

--- Reactor Project/Reactor.py
+++ Reactor Project/Reactor.py
@@ -2,4 +2,6 @@
 
 #This comment should show up on the online file
 
-#I am going to try to branch and then pull on this project change this value to 100 when you get this on desktop: 2
+#I am going to try to branch and then pull on this project change this value to 100 when you get this on desktop: 100
+
+#Not quite sure how it works yet but still testing these comments will be left in for educational tracking.
