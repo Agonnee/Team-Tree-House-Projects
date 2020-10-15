a4666eec99acb514b6916d3eacaafd4aeb3ae985
Agonnee <67187469+Agonnee@users.noreply.github.com>
2020-10-15 07:19:06 -0700

    Added comment on how branches work
    Still learning the interface, that way when I am building an actual project is not the first time I've used the features.

--- Reactor Project/Reactor.py
+++ Reactor Project/Reactor.py
@@ -5,3 +5,7 @@
 #I am going to try to branch and then pull on this project change this value to 100 when you get this on desktop: 100
 
 #Not quite sure how it works yet but still testing these comments will be left in for educational tracking.
+
+'''This comment is left in the first branch I've made from the github desktop.
+Branches are for the entire repository, not just an individual folder within
+Time to learn pull requests and pushing to the branch.'''
