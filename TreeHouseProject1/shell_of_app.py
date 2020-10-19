7eb1f9206c8dadf4a384e3ed6b82c2dcae97e027
Agonnee <67187469+Agonnee@users.noreply.github.com>
2020-10-18 22:08:27 -0700

    Create shell_of_app.py
    This is a simple structure that would describe an outline of a simple application, as far as I am aware.

---
+++ TreeHouseProject1/shell_of_app.py
@@ -0,0 +1,16 @@
+#import space
+
+#Function def space
+
+#Variable establishment
+program_run = True
+
+#on run
+print('Welcome to the simulation')
+
+#main loop
+
+while program_run == True:
+    input('What action would you like to take? ')
+
+
