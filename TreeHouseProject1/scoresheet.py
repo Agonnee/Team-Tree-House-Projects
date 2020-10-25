7f487f7a5cf01fe97ef48a248bda4413f94e403d
Agonnee <67187469+Agonnee@users.noreply.github.com>
2020-10-24 22:41:31 -0700

    updated files from Oct 24th Team Tree house course work

---
+++ TreeHouseProject1/scoresheet.py
@@ -0,0 +1,38 @@
+class YatzyScoresheet:
+    
+    def score_ones(self, hand):
+        return sum(hand.ones)
+
+    def score_twos(self, hand):
+        return sum(hand.twos)
+
+    def score_threes(self, hand):
+        return sum(hand.threes)
+
+    def score_fours(self, hand):
+        return sum(hand.fours)
+
+    def score_fives(self, hand):
+        return sum(hand.fives)
+
+    def score_sixes(self, hand):
+        return sum(hand.sixes)
+
+    def _score_set(self, hand, set_size):
+        scores = [0]
+        for worth, count in hand._sets.items():
+            if count == set_size:
+                scores.append(worth*set_size)
+            return max(scores)
+
+    def score_one_pair(self, hand):
+        return self._score_set(hand, 2)
+
+    def score_three_of_a_kind(self, hand):
+        return self._score_set(hand, 3)
+
+    def score_four_of_a_kind(self, hand):
+        return self._score_set(hand, 4)
+
+    def score_Yatzy(self, hand):
+        return self._score_set(hand, 5)
